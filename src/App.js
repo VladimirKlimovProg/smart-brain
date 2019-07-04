e64900b215939d22764ad57e4c07d1fa89736a01
Vladimir Klimov <vladimirklimoff@yandex.ru>
2019-07-04 17:11:16 +0500

    change server

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 const particlesOptions = {
   particles: {
     number: {
-      value: 150,
+      value: 80,
       density: {
         enable: true,
         value_area: 800
@@ -22,37 +22,39 @@ const particlesOptions = {
 }
 
 const intitialState = {
-      input:'',
-      imageUrl: '',
-      box:{},
-      route:'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = intitialState;
   }
 
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
-    }})
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
   }
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
+    this.setState({ input: event.target.value });
   }
 
   calculateFaceLocation = (data) => {
@@ -72,31 +74,31 @@ class App extends Component {
 
   displayFaceBox = (box) => {
     console.log(box);
-    this.setState({box: box});
+    this.setState({ box: box });
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-      fetch(' https://murmuring-gorge-41341.herokuapp.com/imageurl', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              input: this.state.input
-            })
-          })
+    this.setState({ imageUrl: this.state.input });
+    fetch('https://agile-plateau-81549.herokuapp.com/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://murmuring-gorge-41341.herokuapp.com/image', {
+          fetch('https://agile-plateau-81549.herokuapp.com/image', {
             method: 'put',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
               id: this.state.user.id
             })
           })
             .then(response => response.json())
             .then(count => {
-                this.setState(Object.assign(this.state.user, { entries: count}))            
+              this.setState(Object.assign(this.state.user, { entries: count }))
             })
             .catch(console.log)
 
@@ -108,37 +110,37 @@ class App extends Component {
 
 
   onRouteChange = (route) => {
-    if (route === 'signout'){
+    if (route === 'signout') {
       this.setState(intitialState)
-    } else if (route === 'home'){
-      this.setState({isSignedIn: true})
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
     }
-    this.setState({route: route});
+    this.setState({ route: route });
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, box} = this.state;
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className='particles'
           params={particlesOptions}
         />
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
-        { this.state.route === 'home' 
-        ? <div>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {this.state.route === 'home'
+          ? <div>
             <Logo />
-            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
-            <ImageLinkForm 
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
+            <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
             />
-            <FaceRecognition box={box} imageUrl={imageUrl}/>
+            <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
-        : (
-            route === 'signin' 
-            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-          )      
+          : (
+            route === 'signin'
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          )
         }
       </div>
     );
